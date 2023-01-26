56ac44242e59666f9168e87bd48d943b7802b37a
Fernando Alejandro Yerena Ramos <ferdinandalexandros@gmail.com>
2023-01-26 00:44:06 -0600

    refactor(a11y): add label to inputs

--- src/components/property-adjustment.jsx
+++ src/components/property-adjustment.jsx
@@ -2,7 +2,7 @@ import { useAtom } from 'jotai';
 import { chatStylesAtom } from '../store/stream-chat-styles';
 import debounce from 'just-debounce-it';
 
-function PropertyAdjustment ({ title, properties, type, ...props }) {
+function PropertyAdjustment ({ title, label, properties, type, ...props }) {
   const [chatStyles, setChatStyles] = useAtom(chatStylesAtom);
 
   const handleChange = debounce((e) => {
@@ -17,16 +17,18 @@ function PropertyAdjustment ({ title, properties, type, ...props }) {
     <li className='adjustment'>
       <h3 className='capitalize'>{title}</h3>
       <div>
-        {properties.map((property) =>
-          <input
-            key={`${title}-${property}`}
-            className='input'
-            type={type}
-            data-property={property}
-            defaultValue={chatStyles[property]}
-            onChange={handleChange}
-            {...props}
-          />
+        {properties.map((property, index) =>
+          <label className='capitalize' key={`${title}-${property}`}>
+            <span>{label[index]}</span>
+            <input
+              className='input'
+              type={type}
+              data-property={property}
+              defaultValue={chatStyles[property]}
+              onChange={handleChange}
+              {...props}
+            />
+          </label>
         )}
       </div>
     </li>
--- src/components/settings-panel.jsx
+++ src/components/settings-panel.jsx
@@ -5,9 +5,24 @@ function SettingsPanel () {
   return (
     <aside className='setting-panel'>
       <PropertySet name='chat'>
-        <PropertyAdjustment type='text' title='background' properties={['background-color']} />
-        <PropertyAdjustment type='number' title='padding' properties={['padding-inline', 'padding-block']} min={0} />
-        <PropertyAdjustment type='number' title='spacing' properties={['margin-bottom']} min={0} />
+        <PropertyAdjustment
+          type='text'
+          title='background'
+          label={['color']}
+          properties={['background-color']}
+        />
+        <PropertyAdjustment
+          type='number'
+          title='padding'
+          label={['horizontal', 'vertical']}
+          properties={['padding-inline', 'padding-block']} min={0}
+        />
+        <PropertyAdjustment
+          type='number'
+          title='spacing'
+          label={['margin']}
+          properties={['margin-bottom']} min={0}
+        />
       </PropertySet>
     </aside>
   );
